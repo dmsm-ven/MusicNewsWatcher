a03c384621f5042b565a469363cea3c92bfeed8b
Painven <painven@gmail.com>
2026-03-12 14:05:41 +0300

    web проект удален

--- MusicNewsWatcher.Web/src/api/client.ts/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AlbumDto';
-export * from './ArtistDto';
-export * from './CreateArtistDto';
-export * from './MusicProviderDto';
-export * from './TrackDownloadHistoryDto';
-export * from './TrackDownloadHistoryRequest';
